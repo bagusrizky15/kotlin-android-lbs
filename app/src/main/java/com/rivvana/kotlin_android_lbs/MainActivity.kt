434fd88899f473f5430c4d817e0f724e57de2045
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-11-07 06:09:38 +0700

    Update MainActivity.kt

--- app/src/main/java/com/rivvana/kotlin_android_lbs/MainActivity.kt
+++ app/src/main/java/com/rivvana/kotlin_android_lbs/MainActivity.kt
@@ -4,10 +4,12 @@ import android.content.pm.PackageManager
 import android.location.Geocoder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.rivvana.kotlin_android_lbs.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var geocoder: Geocoder
@@ -17,7 +19,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        geocoder = Geocoder(this, )
+
+        geocoder = Geocoder(this, Locale.getDefault()) //initialize
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
         binding.btnGetLocation.setOnClickListener {
             fetchLocation()
@@ -26,6 +29,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun fetchLocation() {
         val task = fusedLocationProviderClient.lastLocation
+
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
         != PackageManager.PERMISSION_GRANTED && ActivityCompat
                 .checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
@@ -35,7 +39,9 @@ class MainActivity : AppCompatActivity() {
         }
         task.addOnSuccessListener {
             if (it != null){
-                binding.tvLocation.text = "${it.latitude} ${it.longitude}"
+                val address = geocoder.getFromLocation(it.latitude ,it.longitude, 1)
+                binding.tvLocation.text = "${address.get(0).getAddressLine(0)}"
+                Log.e("CEK LOKASI", "${it.latitude} ${it.longitude}")
             }
         }
     }
