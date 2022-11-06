685c0cdf58199a2315255d01bc1ab04c0dad8823
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-11-07 05:33:28 +0700

    .

---
+++ app/src/main/java/com/rivvana/kotlin_android_lbs/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.rivvana.kotlin_android_lbs
+
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.rivvana.kotlin_android_lbs.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        binding.btnGetLocation.setOnClickListener {
+            fetchLocation()
+        }
+    }
+
+    private fun fetchLocation() {
+        val task = fusedLocationProviderClient.lastLocation
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+        != PackageManager.PERMISSION_GRANTED && ActivityCompat
+                .checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        ){
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION ), 101)
+            return
+        }
+        task.addOnSuccessListener {
+            if (it != null){
+                binding.tvLocation.text = "${it.latitude} ${it.longitude}"
+            }
+        }
+    }
+}
